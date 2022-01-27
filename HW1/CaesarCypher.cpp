da8c3ee927198438a3f598342efe805da7026711
CheggStudyTools <alex.zhao@yahoo.com>
2022-01-27 01:03:30 -0500

    homework1

---
+++ HW1/CaesarCypher.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main(){
+    ifstream enc("encrypted.txt");
+
+    if (!enc){
+        cerr << "failed to open encrypted.txt";
+        exit(1);
+    }
+    
+    string line;
+    string ind;
+    int rotate;
+    vector<string> lines;
+    vector<string> rev;
+
+    int rotate;
+    enc >> rotate;
+    cout << rotate << endl;
+    //Problem is here, trying to redefine rotate
+
+    while (getline(enc, line)){
+        lines.push_back(line);
+    }
+    
+    for (size_t i = lines.size(); i > 0; --i){
+        string curr = lines[i-1];
+        for (char ind:curr){
+            if (islower(ind)){
+
+            }
+            //cout << ind << ' ';
+        }
+        //rev.push_back(lines[i-1]);
+        cout << lines[i-1] << endl;
+    }
+
+}
