a92033b685db86100e62c2195c10acd5dfcf1cbf
CheggStudyTools <alex.zhao@yahoo.com>
2022-01-31 12:26:54 -0500

    final

--- HW1/hw01.cpp
+++ HW1/hw01.cpp
@@ -4,6 +4,9 @@
 #include <fstream>
 using namespace std;
 
+char decrypt (char& curr_key, int rotate);
+void stringDecrypt (string& line, int rotate);
+
 int main(){
     ifstream enc("encrypted.txt");
 
@@ -25,15 +28,27 @@ int main(){
 
     for (size_t i = lines.size(); i > 0; --i){
         string curr = lines[i-1];
-        for (char &ind:curr){
-            if (islower(ind)){
-                ind -= rotate;
-                if (ind < 'a'){
-                    ind += 26;
-                }
-            }
-        }
+        stringDecrypt(curr, rotate);
         cout << curr << endl;
     }
     enc.close();
+}
+
+char decrypt(char& curr_key, int rotate){
+    curr_key -= rotate;
+
+    if (curr_key < 'a'){
+        curr_key += 26;
+    } else{}
+    return curr_key;
+}
+
+void stringDecrypt(string& line, int rotate){
+    for (size_t i = 0; i < line.size(); i++)
+    {
+        if (line[i] > 'a' && line[i] < 'z'){
+            line[i] = decrypt(line[i], rotate);
+        } 
+    }
+    
 }
