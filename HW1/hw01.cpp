e7143d9d9b6dcff101ba6e3ccb8c87e47f3b120e
CheggStudyTools <alex.zhao@yahoo.com>
2022-01-31 12:00:30 -0500

    final

--- HW1/hw01.cpp
+++ HW1/hw01.cpp
@@ -23,32 +23,18 @@ int main(){
     while (getline(enc, line)){
         lines.push_back(line);
     }
-    
+
     for (size_t i = lines.size(); i > 0; --i){
         string curr = lines[i-1];
         for (char &ind:curr){
             if (islower(ind)){
-                //cout << "lower";
                 ind -= rotate;
-                //rotating decode by the value at the top of the file
                 if (ind < 'a'){
-                    //cout << "less than";
                     ind += 26;
-
-                    //cout << ind;
-                    //If you rotate to a value below a, you loop back up to z and go from there
-                }else{
-                    //If it doesn't need to loop back then print as usual
-                    //cout << ind;
                 }
-            }else{
-                //Printing all non lowercase characters
-                //cout << ind;
             }
         }
-        //Creating a new line every time you finish decoding and printing out the line
         cout << curr << endl;
-        //cout << endl;
     }
     enc.close();
 }
