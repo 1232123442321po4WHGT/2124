9a7cdad4b80aed76369e78f00feabfa014873964
CheggStudyTools <alex.zhao@yahoo.com>
2022-02-07 16:12:27 -0500

    start

---
+++ HW2/hw02.cpp
@@ -0,0 +1,12 @@
+/*
+Alex Zhao
+This program pits warriors against each other and computes their
+respective strengths after each engagement
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
