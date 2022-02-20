bd02c714b895cf485c14e47c82829f00729d9ab6
CheggStudyTools <alex.zhao@yahoo.com>
2022-02-20 17:21:02 -0500

    start

---
+++ HW4/hw04.cpp
@@ -0,0 +1,11 @@
+// Alex Zhao
+// This progam is an extension of the third homework
+// this time using pointers
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
