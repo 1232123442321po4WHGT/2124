52c4a0b80578d20b9e4eb719f99329b6d3e4febe
CheggStudyTools <alex.zhao@yahoo.com>
2022-02-26 20:22:12 -0500

    starter code

---
+++ HW5/hw05.cpp
@@ -0,0 +1,13 @@
+//hw05.cpp Alex Zhao
+//This homework is a continuation of the fourth homework with some 
+//differences, namely using the heap. 
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+class Warrior{
+    
+};
