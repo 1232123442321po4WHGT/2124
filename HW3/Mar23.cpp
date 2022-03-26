e952bab9d28901ded025ec5eb570999e6c5632ca
CheggStudyTools <alex.zhao@yahoo.com>
2022-03-26 00:16:26 -0400

    notes

--- HW3/Mar23.cpp
+++ HW3/Mar23.cpp
@@ -4,3 +4,42 @@
 #include <fstream>
 using namespace std;
 
+class Vehicle{
+    public:
+    void roll(){
+        cout << "rolling" << endl;
+    }
+};
+class LandVehicle : public Vehicle{};
+class WaterVehicle : public Vehicle{};
+class JetSki : public WaterVehicle{};
+class Car : public Vehicle{};
+class Helicopter : public Vehicle{};
+
+class Animal{
+    public:
+    void eat(){
+        cout << "Animal is eating" << endl;
+    }
+};
+
+class Lion : public Animal{};
+class Tiger : public Animal{
+    public:
+    void eat(){
+        cout << "Tiger is eating" << endl;
+    }
+};
+class Bear : public Animal{};
+
+int main(){
+    Bear yogi;
+    yogi.eat();
+
+    Tiger tigger;
+    tigger.eat();
+
+    Vehicle car;
+    car.roll();
+}
+
